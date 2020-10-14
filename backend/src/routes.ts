a93bcbbf5d36c6e0b3793fe7920d1cf35d1ae4f2
Emerson Duarte <raasjava@gmail.com>
2020-10-14 10:20:28 -0300

    upload de imagens

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,13 @@
 import {Router} from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
 import OrphanagesController from './controllers/OrphanagesControllers';
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
-routes.post('/orphanages', OrphanagesController.create);
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
 routes.get('/orphanages/:id', OrphanagesController.show);
 routes.get('/orphanages', OrphanagesController.index);
 
